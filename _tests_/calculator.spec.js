ba04fe03332eec6229b6494d8f12f8a9092fae3c
Alienah <aidalbla@gmail.com>
2018-05-24 16:27:04 +0200

    red: check if the values are numbers

--- _tests_/calculator.spec.js
+++ _tests_/calculator.spec.js
@@ -1,11 +1,17 @@
 import calculator from "../src/index";
-
+let app;
+beforeEach(function () {
+    app = calculator();
+});
 describe("calculator app", () => {
-    let app;
-    beforeEach(function () {
-        app = calculator();
-    });
+    
     it("sum two numbers", () => {
         expect(app.sum(1,1)).toBe(2);
+        
+    });
+});
+describe("check values", () => {
+    it("check if the value introduced is type number", () => {
+        expect(app.sum(2 + "2")).toBe(console.log("Debes introducir un número"));
     });
 });
